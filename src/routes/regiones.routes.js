b142dcf1f25b092fa13b21c82db186b9f76b7116
climoralesg <38842608+climoralesg@users.noreply.github.com>
2020-05-28 18:56:55 -0400

    obtener region por cod iso

--- src/routes/regiones.routes.js
+++ src/routes/regiones.routes.js
@@ -17,10 +17,37 @@ router.get('/datos', async (req, res) => {
 
 router.get('/', async (req, res) => {
     const db = await connect();
-    const busqueda = await db.collection('regions').find({},
-        { projection: { "region": 1, "capital_regional": 1, "region_iso_3166_2": 1 } }).toArray();
-    console.log(busqueda);
-    res.json(busqueda);
+
+    if (db === 1) {
+        const respuesta = mensajeArray(1, "Hubo un error en la conexion a la base de datos", null)
+        console.log(respuesta);
+        res.json(respuesta);
+    } else {
+        try{
+            const busqueda = await db.collection('regions').find({},
+                { projection: { "region": 1, "capital_regional": 1, "region_iso_3166_2": 1 } })
+                .toArray().then(function(docs){
+                    if(docs.length==0){
+                        const respuesta = mensajeArray(3, "Consulta relizada, no se ha encontrado documento", null)
+                        console.log(respuesta);
+                        res.json(respuesta);
+                    }else{
+                        const respuesta = mensajeArray(0, "Consulta relizada", docs)
+                        console.log(respuesta);
+                        res.json(respuesta);
+                    }
+            });
+            
+            //console.log(busqueda);
+            //res.json(busqueda);
+            
+        }catch(error){
+            const respuesta = mensajeArray(2, "Hubo un error en la consulta", null);
+            console.log(respuesta);
+            res.json(respuesta);
+        }
+
+    }
 
 });
 
@@ -28,53 +55,89 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const db = await connect();
     if (db === 1) {
-        const estado = {
-            estado: {
-                codigo: db,
-                respuesta: 'Error la conexion a la base de datos'
-            },
-            region: null
-        }
-        res.json(estado);
+        const respuesta = mensaje(1, "Hubo un error en la conexion a la base de datos", null)
+        console.log(respuesta);
+        res.json(respuesta);
     } else {
         try {
-            await db.collection('regions').findOne({ _id: ObjectID(id) }).then(function(doc){
-                if(!doc){
-                    const estado = {
-                        estado: {
-                            codigo: 3,
-                            respuesta: 'Consulta realizada, no se ha encontrado documento'
-                        },
-                        region:null
-                    }
-                    console.log(estado);
-                    res.json(estado);
-                }else{
-                    const estado = {
-                        estado: {
-                            codigo: 0,
-                            respuesta: 'Consulta realizada'
-                        },
-                        region: {
-                            ...doc
-                        }
-                    }
-                    console.log(estado);
-                    res.json(estado);
+            const busqueda =await db.collection('regions').findOne({ _id: ObjectID(id) }).then(function (doc) {
+                if (!doc) {
+                    const respuesta = mensaje(3, "Consulta relizada, no se ha encontrado documento", null)
+                    console.log(respuesta);
+                    res.json(respuesta);
+                } else {
+                    const respuesta = mensaje(0, "Consulta relizada", doc)
+                    console.log(respuesta);
+                    res.json(respuesta);
                 }
             });
+            console.log(busqueda);
         } catch (error) {
-            const estado = {
-                estado: {
-                    codigo: 2,
-                    respuesta: 'Hubo un error en la consulta'
-                },
-                region: null
-            }
-            res.json(estado);
+            const respuesta = mensaje(2, "Hubo un error en la consulta", null);
+            console.log(respuesta);
+            res.json(respuesta);
         }
     }
     db.close;
 });
+
+router.get('/cod_iso/:cod_iso',async(req,res)=>{
+    const { cod_iso } = req.params;
+    const codigo=cod_iso.toUpperCase();
+    const db = await connect();
+    if (db === 1) {
+        const respuesta = mensaje(1, "Hubo un error en la conexion a la base de datos", null)
+        console.log(respuesta);
+        res.json(respuesta);
+    }else{
+        try {
+            const busqueda =await db.collection('regions').findOne({ region_iso_3166_2: codigo}).then(function (doc) {
+                if (!doc) {
+                    const respuesta = mensaje(3, "Consulta relizada, no se ha encontrado documento", null)
+                    console.log(respuesta);
+                    res.json(respuesta);
+                } else {
+                    const respuesta = mensaje(0, "Consulta relizada", doc)
+                    console.log(respuesta);
+                    res.json(respuesta);
+                }
+            });
+            //console.log(busqueda);
+        } catch (error) {
+            const respuesta = mensaje(2, "Hubo un error en la consulta", null);
+            console.log(respuesta);
+            res.json(respuesta);
+        }
+    }
+});
+
+
+function mensaje(cod, resp, info) {
+    const estado = {
+        estado: {
+            codigo: cod,
+            respuesta: resp,
+        },
+        region: {
+            ...info
+        }
+    }
+    return estado;
+}
+
+function mensajeArray(cod, resp, info) {
+    const estado = {
+        estado: {
+            codigo: cod,
+            respuesta: resp,
+        },
+        regiones: [
+            ...info
+        ]
+    }
+    return estado;
+}
+
+
 
 export default router;
